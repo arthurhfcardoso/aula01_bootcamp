ed8aeae8abd3884f709e3e5594a59fd247e7e4ee
arthurhfcardoso <arthurhfcardoso@gmail.com>
2026-01-08 08:52:54 -0300

    Commit das alterações da aula01 do bootcamp Python da Jornada de Dados

--- exemplo_00.py
+++ exemplo_00.py
@@ -2,4 +2,9 @@ print("hello world")
 
 # Programa que soma o número de caracteres do nome
 
-print(len(input("Digite seu nome: "))) + input
+nome = input("Digite seu nome: ")
+print(f"Seu nome tem {len(nome)} caracteres.")
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+print(f"A soma é {a + b}")
